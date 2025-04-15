c7c01e28a5cd5cc1edca6588bbf46e986b7079d5
shubkb07 <shub@shubkb.com>
2025-04-16 01:46:43 +0530

    refactor: Update minimum required WordPress version to 6.8

--- plugins/wisesync/wisesync.php
+++ plugins/wisesync/wisesync.php
@@ -9,7 +9,7 @@
  * Plugin URI:        https://shubkb.com
  * Description:       All-in-one solution for WordPress users to use everything WiseSync has to offer to make them successful on the web.
  * Version:           1.0.0
- * Requires at least: 6.6
+ * Requires at least: 6.8
  * Requires PHP:      8.3
  * Author:            Shubham Kumar Bansal <shub@shubkb.com>
  * Author URI:        https://shubkb.com
