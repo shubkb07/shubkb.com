918f697e52368ca43548f65def5fedd7dbcba88c
shubkb07 <shub@shubkb.com>
2025-04-15 22:12:47 +0530

    Remove incompatible plugin configurations and dynamic lists from WP Rocket settings

--- wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1743589852;s:7:"authKey";s:64:"*{gM}IK`MBa2<C%`rx,N%l,ngoLN4I_+1z2?mETM~21zh;hr8{TNXn0@+<g:C^W]";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3833;}
