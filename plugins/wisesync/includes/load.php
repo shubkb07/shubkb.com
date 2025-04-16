b8ca9fae5873a4fb43cbd533c4db826644f3f58b
shubkb07 <shub@shubkb.com>
2025-04-17 02:18:27 +0530

    feat: Add cookie banner block and load plugin functionality

---
+++ plugins/wisesync/includes/load.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Load WiseSync Plugin
+ *
+ * This file is responsible for loading the WiseSync plugin.
+ *
+ * @package WISESYNC
+ * @since   1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Register Blocks Category.
+add_filter( 'block_categories_all', 'register_sync_blocks_category' );
+
+// Load Plugin Blocks.
+add_action(
+	'init',
+	function () {
+		register_sync_block_type( WSYNC_PLUGIN_DIR . 'blocks/build/' );
+	}
+);
