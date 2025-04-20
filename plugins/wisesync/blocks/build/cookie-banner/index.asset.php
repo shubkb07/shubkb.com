a45f726314e6065eb1d0d6843680bbab644a61f1
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-20 08:40:14 +0000

    Deploy build: 2025-04-20 08:40:13 - Refactor cookie banner block to clarify footer template part check and update warning message

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++ plugins/wisesync/blocks/build/cookie-banner/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n' ),
-	'version'      => '4491fd95be75d441a303',
+	'version'      => 'd267d455de338ee85a7f',
 );
