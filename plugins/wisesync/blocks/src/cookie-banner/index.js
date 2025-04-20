cad81afd0c1be014c12715d80913f9bcf1608bc3
shubkb07 <shub@shubkb.com>
2025-04-20 19:50:27 +0530

    Refactor cookie banner block registration to simplify edit and save functions, removing footer template checks and warnings.

--- plugins/wisesync/blocks/src/cookie-banner/index.js
+++ plugins/wisesync/blocks/src/cookie-banner/index.js
@@ -13,47 +13,29 @@ import { __ } from '@wordpress/i18n';
 /**
  * Register the block
  */
-registerBlockType('my-plugin/footer-only-block', {
-  edit: ({ context }) => {
-    const blockProps = useBlockProps();
-    
-    // Check if we're in a footer template part
-    const isFooter = context?.templateSlug === 'footer';
-    
-    // Check if this block already exists in the current template
-    const blockCount = useSelect((select) => {
-      const blocks = select('core/block-editor').getBlocks();
-      return blocks.filter(block => block.name === 'my-plugin/footer-only-block').length;
-    }, []);
-    
-    // If we're not in a footer template or the block is already used, show a warning
-    if (!isFooter) {
-      return (
-        <div {...blockProps} className="footer-only-block-warning">
-          <p>{__('This block can only be used in footer template parts.', 'my-plugin')}</p>
-        </div>
-      );
-    }
-    
-    // Normal block content
-    return (
-      <div {...blockProps}>
-        <div className="footer-only-block">
-          <InnerBlocks />
-        </div>
-      </div>
-    );
-  },
-  
-  save: () => {
-    const blockProps = useBlockProps.save();
-    
-    return (
-      <div {...blockProps}>
-        <div className="footer-only-block">
-          <InnerBlocks.Content />
-        </div>
-      </div>
-    );
-  },
-});
+registerBlockType( 'my-plugin/footer-only-block', {
+	edit: ( { } ) => {
+		const blockProps = useBlockProps();
+
+		// Normal block content
+		return (
+			<div { ...blockProps }>
+				<div className="footer-only-block">
+					<InnerBlocks />
+				</div>
+			</div>
+		);
+	},
+
+	save: () => {
+		const blockProps = useBlockProps.save();
+
+		return (
+			<div { ...blockProps }>
+				<div className="footer-only-block">
+					<InnerBlocks.Content />
+				</div>
+			</div>
+		);
+	},
+} );
