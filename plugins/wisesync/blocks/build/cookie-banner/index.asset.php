e7c2eec91e8fff5b8eb178f1929727f888cad12c
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-20 14:21:47 +0000

    Deploy build: 2025-04-20 14:21:47 - Refactor cookie banner block registration to simplify edit and save functions, removing footer template checks and warnings.

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++ plugins/wisesync/blocks/build/cookie-banner/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n' ),
-	'version'      => '30b82ba22ad21b7736ac',
+	'version'      => '25bfcca8bfb9de97b159',
 );
--- plugins/wisesync/blocks/build/cookie-banner/index.js
+++ plugins/wisesync/blocks/build/cookie-banner/index.js
@@ -1,12 +1,9 @@
 ( () => {
 	'use strict'; const o = window.wp.blocks,
-		e = window.wp.data,
-		n = window.wp.blockEditor,
-		l = window.wp.i18n,
-		s = window.ReactJSXRuntime; ( 0, o.registerBlockType )( 'my-plugin/footer-only-block', { edit: ( { context: o } ) => {
-		const t = ( 0, n.useBlockProps )(),
-			c = 'footer' === o?.templateSlug; return ( 0, e.useSelect )( ( ( o ) => o( 'core/block-editor' ).getBlocks().filter( ( ( o ) => 'my-plugin/footer-only-block' === o.name ) ).length ), [] ), c ? ( 0, s.jsx )( 'div', { ...t, children: ( 0, s.jsx )( 'div', { className: 'footer-only-block', children: ( 0, s.jsx )( n.InnerBlocks, {} ) } ) } ) : ( 0, s.jsx )( 'div', { ...t, className: 'footer-only-block-warning', children: ( 0, s.jsx )( 'p', { children: ( 0, l.__ )( 'This block can only be used in footer template parts.', 'my-plugin' ) } ) } );
+		n = ( window.wp.data, window.wp.blockEditor ),
+		e = ( window.wp.i18n, window.ReactJSXRuntime ); ( 0, o.registerBlockType )( 'my-plugin/footer-only-block', { edit: ( {} ) => {
+		const o = ( 0, n.useBlockProps )(); return ( 0, e.jsx )( 'div', { ...o, children: ( 0, e.jsx )( 'div', { className: 'footer-only-block', children: ( 0, e.jsx )( n.InnerBlocks, {} ) } ) } );
 	}, save: () => {
-		const o = n.useBlockProps.save(); return ( 0, s.jsx )( 'div', { ...o, children: ( 0, s.jsx )( 'div', { className: 'footer-only-block', children: ( 0, s.jsx )( n.InnerBlocks.Content, {} ) } ) } );
+		const o = n.useBlockProps.save(); return ( 0, e.jsx )( 'div', { ...o, children: ( 0, e.jsx )( 'div', { className: 'footer-only-block', children: ( 0, e.jsx )( n.InnerBlocks.Content, {} ) } ) } );
 	} } );
 } )();
