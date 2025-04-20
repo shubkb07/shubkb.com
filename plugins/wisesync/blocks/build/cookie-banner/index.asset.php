ebb953f0da7e579a83bfd61ba5effedb7725cd00
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-20 08:46:51 +0000

    Deploy build: 2025-04-20 08:46:51 - Add 'inserter' support to cookie banner block configuration

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++ plugins/wisesync/blocks/build/cookie-banner/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n' ),
-	'version'      => 'd267d455de338ee85a7f',
+	'version'      => '0577a3cb7de0c2545322',
 );
