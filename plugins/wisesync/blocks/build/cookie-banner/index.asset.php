62b9fae561ec4712c0f9a3119c113945869a16d7
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-20 09:33:27 +0000

    Deploy build: 2025-04-20 09:33:26 - Refactor cookie banner block configuration for clarity and improve allowed block types filtering in the Site Editor

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++ plugins/wisesync/blocks/build/cookie-banner/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n' ),
-	'version'      => 'c4b8ad11feb2ed8178ad',
+	'version'      => 'e05e470eb23d09c32fca',
 );
