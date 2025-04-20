ef30213ec107641349ec841d8de1c27eb205c052
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-20 08:50:26 +0000

    Deploy build: 2025-04-20 08:50:26 - Remove inserter support from cookie banner block configuration and improve footer area check in edit component

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++ plugins/wisesync/blocks/build/cookie-banner/index.asset.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'dependencies' => array( 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n' ),
-	'version'      => '0577a3cb7de0c2545322',
+	'version'      => 'c4b8ad11feb2ed8178ad',
 );
