161809dff590d17d00d1d194c46ec4fe11cee792
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-22 09:03:21 +0000

    Deploy build: 2025-04-22 09:03:21 - Add static asset registration and enqueue functions for event styles

---
+++ plugins/wisesync/assets/js/event.js
@@ -0,0 +1,8 @@
+const a = {};
+const b = document.getElementsByTagName( 'html' )[ 0 ];
+const registerEvent = ( eventName, condition, callback ) => {
+
+};
+b.addEventListener( 'click', ( e ) => {
+	console.log( e );
+} );
