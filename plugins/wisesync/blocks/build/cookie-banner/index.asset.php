fdc65a15367dd7712192e20f817688b115d61c04
Shubham Kumar Bansal <shub@shubkb.com>
2025-04-23 08:04:37 +0000

    Deploy build: 2025-04-23 08:04:37 - chore: update build workflow to version 1.2 and enhance dependency detection logic

--- plugins/wisesync/blocks/build/cookie-banner/index.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array( 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n' ),
-	'version'      => '35f12c87ed35795ea5d6',
-);
--- plugins/wisesync/blocks/build/cookie-banner/view.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return array(
-	'dependencies' => array(),
-	'version'      => 'e7001ec9483634b7968e',
-);
