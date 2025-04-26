6054b49632ce0ed22c1f1f52dbc7a1009762c000
shubkb07 <shub@shubkb.com>
2025-04-26 23:31:15 +0530

    Refactor menu handling in Sync_Settings class to improve submenu registration and settings level management

--- plugins/wisesync/includes/functions/settings.php
+++ plugins/wisesync/includes/functions/settings.php
@@ -22,4 +22,16 @@ $settings = new Sync_Settings();
  *
  * @since 1.0.0
  */
-function sync_add_settings_menu() {}
+function sync_add_wp_settings_menu( $menu_slug, $menu_name, $position = 100, $create_sync_menu = true, $settings_level = 'site' ) {
+
+    /**
+     * Sync Settings Class
+     *
+     * @var \Sync\Sync_Settings
+     */
+    global $settings;
+
+    $settings->add_wp_menu( $menu_slug, $menu_name, $position, $create_sync_menu, $settings_level );
+}
+
+sync_add_wp_settings_menu( 'wisesync-settings', __( 'WiseSync Settings', 'wisesync' ), 10, true, 'site' );
