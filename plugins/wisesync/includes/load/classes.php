34bab17030e7ed4fa30af13a7d522172241a6d1f
Shubham Kumar Bansal <shub@shubkb.com>
2025-05-08 19:46:57 +0000

    Deploy build: 2025-05-08 19:46:57 – feat: Add Sync_Post, Sync_Site_Health, and Sync_Remote_Request classes for post operations, site health checks, and remote requests   (SHA:12c1a1b89f6a0bb1f081ef5ac16818f77bb58732)

--- plugins/wisesync/includes/load/classes.php
+++ plugins/wisesync/includes/load/classes.php
@@ -6,13 +6,16 @@
  * @since    1.0.0
  */
 
-use Sync\{Sync_Settings, Sync_Ajax, Sync_CLI, Sync_Filesystem};
+use Sync\{Sync_Settings, Sync_Ajax, Sync_CLI, Sync_Filesystem, Sync_Site_Health, Sync_Remote_Request, Sync_Post};
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
  
-$sync_ajax       = new Sync_Ajax();
-$sync_settings   = new Sync_Settings();
-$sync_cli        = new Sync_CLI();
-$sync_filesystem = new Sync_Filesystem();
+$sync_ajax           = new Sync_Ajax();
+$sync_settings       = new Sync_Settings();
+$sync_cli            = new Sync_CLI();
+$sync_filesystem     = new Sync_Filesystem();
+$sync_site_health    = new Sync_Site_Health();
+$sync_remote_request = new Sync_Remote_Request();
+$sync_post           = new Sync_Post();
