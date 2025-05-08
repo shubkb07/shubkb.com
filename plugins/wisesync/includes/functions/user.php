c59bf146ef1a295881475e9fe91b13f500490818
Shubham Kumar Bansal <shub@shubkb.com>
2025-05-08 19:56:32 +0000

    Deploy build: 2025-05-08 19:56:32 – feat: Add Sync_Helpers and Sync_User classes along with helper functions for improved plugin functionality   (SHA:b2083b04771c3fb80838eb193ba135c74c27374b)

---
+++ plugins/wisesync/includes/functions/user.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * WiseSync User Functions
+ *
+ * This file contains functions for user-related operations in the WiseSync plugin.
+ *
+ * @package   WiseSync
+ * @since     1.0.0
+ */
--- plugins/wisesync/includes/load/classes.php
+++ plugins/wisesync/includes/load/classes.php
@@ -6,12 +6,13 @@
  * @since    1.0.0
  */
 
-use Sync\{Sync_Settings, Sync_Ajax, Sync_CLI, Sync_Filesystem, Sync_Site_Health, Sync_Remote_Request, Sync_Post};
+use Sync\{Sync_Settings, Sync_Ajax, Sync_CLI, Sync_Filesystem, Sync_Site_Health, Sync_Remote_Request, Sync_Post, Sync_User};
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
- 
+
+$sync_helpers        = new Sync_Helpers();
 $sync_ajax           = new Sync_Ajax();
 $sync_settings       = new Sync_Settings();
 $sync_cli            = new Sync_CLI();
@@ -19,3 +20,4 @@ $sync_filesystem     = new Sync_Filesystem();
 $sync_site_health    = new Sync_Site_Health();
 $sync_remote_request = new Sync_Remote_Request();
 $sync_post           = new Sync_Post();
+$sync_user           = new Sync_User();
